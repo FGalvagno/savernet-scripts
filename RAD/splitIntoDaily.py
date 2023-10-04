dcc8e7bf9b2265f1670a1204acd91a128bf7c11d
Facundo Galvagno <galvagno.facundo@outlook.com>
2023-10-04 10:18:42 -0300

    drop df empty columns

--- RAD/splitIntoDaily.py
+++ RAD/splitIntoDaily.py
@@ -34,24 +34,33 @@ def split(location):
     """ Reads *.dat file. Output from Campbell datalogger
         Output: file splited by dates.
     """
-    #TODO: save header
+    
+    # save original header
+    with open("COR_PIRA-UVA-UVB.dat") as input_file:
+        head = [next(input_file) for _ in range(4)]
+    print(head)
+    
+    # read file
     hd = pd.read_csv("COR_PIRA-UVA-UVB.dat")
-
     df = pd.read_csv("COR_PIRA-UVA-UVB.dat", skiprows=[1, 3, 4])
 
+    # set header for df
     df.columns = df.iloc[0]
     df = df[1:]
-    #TODO: drop NaN columns
+    
+    # drop empty columns
+    n=2
+    df = df.iloc[:,:-2]
 
+    # split df into csv, add original header
     df['TS'] = pd.to_datetime(df['TS'])
-
-
-
     print(df.dtypes)
-
     for i in set(df['TS'].dt.date):
+        f = open(f"export/{location+'-'+i.strftime('%Y-%m-%d')}.csv", "w")
+        f.writelines(head)
+        f.close()
         data = df[(df['TS'].dt.date >= i) & (df['TS'].dt.date <= i+timedelta(days=1))]
-        data.to_csv(f"export/{location+'-'+i.strftime('%Y-%m-%d')}.csv",index=False)
+        data.to_csv(f"export/{location+'-'+i.strftime('%Y-%m-%d')}.csv", index=False, mode= 'a', header=False)
 
 location = setup()
 split(location)
